b28314709b4ab23a757cf1ecbfefd82f7e0c7144
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-04-02 18:31:56 +0300

    Added author.py test kit

--- article.py
+++ article.py
@@ -1,9 +1,9 @@
-
-def init(self, title, magazine, topic): self.title = title self.magazine = magazine self.topic = topic
+def init(self,title,magazine,topic):self.title =title
+self.magazine = magazine self.topic
 
 def get_title(self): return self.title
 
-def get_magazine(self): return self.magazine
+def get_magazine(self):return self.magazine
 
-def get_topic(self): return self.topic
+def get_topic(self):return self.topic
 

d06e078a304bada28eba80972551d59ed7b452f3
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-11 12:43:21 +0300

    modified many_to_many.py

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -8,4 +8,13 @@ class Article:
         Article.all.append(self)
 
     @property
-    def title(
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, new_title):
+        if hasattr(self, "title"):
+            AttributeError("Title cannot be changed")
+        else:
+            if isinstance(new_title, str):
+                if 5 <= le
